654062f28628269c40ef3baa832fe66384237923
David Acosta <david.acosta.ing@gmail.com>
2025-05-22 12:04:08 -0500

    Initial entry animation

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,6 +9,7 @@ public class UIManager : MonoBehaviour
     private Button _closeButton;
     private VisualElement _bottomSheet;
     private VisualElement _scrim;
+    private VisualElement _astronaut;
 
     private void Start()
     {
@@ -19,14 +20,25 @@ public class UIManager : MonoBehaviour
         _closeButton = root.Q<Button>("CloseButton");
         _bottomSheet = root.Q<VisualElement>("BottomSheet");
         _scrim = root.Q<VisualElement>("Scrim");
+        _astronaut = root.Q<VisualElement>("Astronaut");
         
         _bottomContainer.style.display = DisplayStyle.None;
         _openButton.clicked += OpenButtonClicked;
         _closeButton.clicked += CloseButtonClicked;
+
+        Invoke("AnimateAstroaut", 0.1f); // Since the first frame does not assign a state in UI, we need to wait a bit before playing the initial animation
     }
+
+    private void AnimateAstroaut()
+    {
+        // Debug.Log($"{_astronaut.ClassListContains("astronaut-OutScreen")}");
+        _astronaut.RemoveFromClassList("astronaut-OutScreen");
+    }
+
     private void OnDestroy()
     {
         _openButton.clicked -= OpenButtonClicked;
+        _closeButton.clicked -= CloseButtonClicked;
     }
     private void OpenButtonClicked()
     {
