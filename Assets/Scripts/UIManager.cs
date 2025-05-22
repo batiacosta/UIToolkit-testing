7be7ab295276dfe71a3c9349253988b401739b48
David Acosta <david.acosta.ing@gmail.com>
2025-05-22 10:20:35 -0500

    Quering and setting up bottom container display none from the beginning

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    private VisualElement _bottomContainer;
+    private Button _openButton;
+    private Button _closeButton;
+
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        if (root == null) return;
+        _bottomContainer = root.Q<VisualElement>("BottomContainer");
+        _openButton = root.Q<Button>("OpenButton");
+        _closeButton = root.Q<Button>("CloseButton");
+        
+        _bottomContainer.style.display = DisplayStyle.None;
+    }
+}
